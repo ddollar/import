bc4d026f3fc5ff2d6cbe2c60ec205c416fdffe04
David Dollar <ddollar@gmail.com>
2009-11-11 15:36:09 -0500

    just import the last constant

--- import.rb
+++ import.rb
@@ -1,7 +1,5 @@
 def import(filename)
   wrapper = Module.new
   wrapper.module_eval(File.read(filename), filename)
-
-  raise 'More than one object defined' unless wrapper.constants.length == 1
-  wrapper.const_get(wrapper.constants.first)
+  wrapper.const_get(wrapper.constants.last)
 end
