ca64c77f2bb69a9f522192ca85c59562dfa51146
David Dollar <ddollar@gmail.com>
2009-11-11 15:27:36 -0500

    remove debug

--- import.rb
+++ import.rb
@@ -1,8 +1,7 @@
 def import(filename)
   wrapper = Module.new
   wrapper.module_eval(File.read(filename), filename)
-  
-  puts wrapper.constants.inspect
+
   raise 'More than one object defined' unless wrapper.constants.length == 1
   wrapper.const_get(wrapper.constants.first)
 end
